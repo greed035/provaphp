f28cd7f3773f6dd9719f781710ae1207f2072a89
queijinha <geanlucas13572500@gmail.com>
2024-12-02 07:58:59 -0300

    provaphp

---
+++ index.php
@@ -0,0 +1,27 @@
+<h2>reseita
+    <?php
+    echo ":arroz e feijao"
+    ?>
+</h2>
+<table>
+    <thead>
+        <tr>
+            <th>ingredientes</th>
+            <th>quantidade</th>
+        </tr> 
+    <thead>
+    <tbody>
+<tr>
+    <td>arroz</td>
+    <td>3 copo</td>
+</tr>
+<tr>
+    <td>feijao</td>
+    <td>3 copo</td>
+</tr>
+</table>
+<h1>nota:</h1>
+<script>
+    var h1=document.querySelector("h1");
+    h1.innerHTML="nota:10";
+</script>
